dd0b28436acd1e274a01329c14af7f5175c63fa3
jlamson <darkmoose117@gmail.com>
2024-06-17 18:58:50 -0600

    Update filtering and randomization logic

--- app/_data/dominion/dominion-kingdoms.ts
+++ app/_data/dominion/dominion-kingdoms.ts
@@ -8,6 +8,34 @@ export class DominionKingdoms {
   static readonly kingdoms: { [key in SetId]?: DominionKingdom[] } =
     DominionKingdoms.createKingdoms();
 
+  private static _allKingdoms: DominionKingdom[] | undefined;
+
+  public static getAllKingdoms(): DominionKingdom[] {
+    if (!this._allKingdoms) {
+      const allKingdoms = Object.values(DominionKingdoms.kingdoms).flat();
+      const uniqueKingdomIds = new Set(
+        allKingdoms.map((kingdom) => kingdom.id)
+      );
+      this._allKingdoms = Array.from(uniqueKingdomIds).map(
+        (id) =>
+          allKingdoms.find((kingdom) => kingdom.id === id) as DominionKingdom
+      );
+    }
+    return this._allKingdoms;
+  }
+
+  public static getKingdomsForSets(setIds: SetId[]): DominionKingdom[] {
+    return this.getAllKingdoms().filter((kingdom) =>
+      setIds.every((setId) => kingdom.setIds.includes(setId))
+    );
+  }
+
+  public static getKingdomById(kingdomId: string): DominionKingdom | null {
+    return (
+      this.getAllKingdoms().find((kingdom) => kingdom.id === kingdomId) ?? null
+    );
+  }
+
   private static createKingdoms() {
     const setIds = Object.keys(allKingdoms) as SetId[];
     const sets: { [key in SetId]?: DominionKingdom[] } = {};
--- app/kingdoms/KingdomList.tsx
+++ app/kingdoms/KingdomList.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import {
+  Button,
   Checkbox,
+  Group,
   MantineProvider,
+  MultiSelect,
   Select,
   Space,
   Stack,
+  Text,
   createTheme,
 } from "@mantine/core";
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { DominionKingdoms } from "../_data/dominion/dominion-kingdoms";
 import { DominionSet } from "../_data/dominion/dominion-set";
 import { DominionSets } from "../_data/dominion/dominion-sets";
 import { SetId } from "../_data/dominion/set-id";
-import { useStorage } from "../_hooks/useStorage";
+import { useStorage, useStoredState } from "../_hooks/useStorage";
 import { KingdomChecklistItem } from "./KingdomChecklistItem";
 import classes from "./KingdomList.module.css";
+import { DominionKingdom } from "../_data/dominion/dominion-kingdom";
 
 const theme = createTheme({
   components: {
@@ -25,40 +30,69 @@ const theme = createTheme({
 
 const sets: DominionSet[] = DominionSets.getAllSets();
 
+const randomFrom = <T,>(arr: T[]): T | null => {
+  return arr.length ? arr[Math.floor(Math.random() * arr.length)] : null;
+};
+
 export const KingdomList: React.FC = () => {
   const storage = useStorage();
-  const [selectedSet, setSelectedSet] = useState<SetId | "random">(
-    SetId.BASE_SET
+  const [specificKingdom, setSpecificKingdom] =
+    useState<DominionKingdom | null>(null);
+  const [selectedSets, setSelectedSets] = useStoredState<SetId[]>(
+    "selectedSets",
+    [SetId.BASE_SET_2]
   );
 
-  const sets = useMemo(() => DominionSets.getAllSets(), []);
+  useEffect(() => {
+    setSpecificKingdom(null);
+  }, [selectedSets, setSpecificKingdom]);
 
-  const kingdoms = useMemo(() => {
-    if (selectedSet === "random") {
-      const allKingdoms = Object.values(DominionKingdoms.kingdoms)
-        .flat()
-        .filter((kingdom) => storage.getItem(kingdom.id) !== "false");
-      return [allKingdoms[Math.floor(Math.random() * allKingdoms.length)]];
+  const sets = useMemo(() => DominionSets.getAllSets(), []);
+  const filteredKingdoms = useMemo(
+    () => DominionKingdoms.getKingdomsForSets(selectedSets),
+    [selectedSets]
+  );
+  const kingdoms: DominionKingdom[] = useMemo(() => {
+    if (specificKingdom) {
+      return [specificKingdom];
     } else {
-      return DominionKingdoms.kingdoms[selectedSet] || [];
+      return filteredKingdoms;
     }
-  }, [selectedSet, storage]);
+  }, [filteredKingdoms, specificKingdom]);
+
+  const setRandomKingdom = useCallback(() => {
+    const initialKingdomList =
+      filteredKingdoms.length > 0
+        ? filteredKingdoms
+        : DominionKingdoms.getAllKingdoms();
+    const incompleteKingdoms = initialKingdomList.filter(
+      (kingdom) => storage.getItem(kingdom.id) !== "true"
+    );
+    const randomKingdom = randomFrom(
+      incompleteKingdoms.length > 0 ? incompleteKingdoms : filteredKingdoms
+    );
+    setSpecificKingdom(randomKingdom);
+  }, [filteredKingdoms, setSpecificKingdom, storage]);
+
   return (
     <MantineProvider theme={theme}>
-      <Select
-        data={[
-          { value: "random", label: "Random" },
-          ...sets.map((set) => ({
+      <Group justify="stretch">
+        <MultiSelect
+          style={{ flexGrow: 1 }}
+          data={sets.map((set) => ({
             value: set.setId,
             label: set.name,
-          })),
-        ]}
-        checkIconPosition="left"
-        value={selectedSet}
-        onChange={(selected) => setSelectedSet(selected as SetId)}
-      />
+          }))}
+          checkIconPosition="left"
+          value={selectedSets}
+          onChange={(values) => setSelectedSets(values as SetId[])}
+          clearable
+        />
+        <Button onClick={setRandomKingdom}>Random</Button>
+      </Group>
       <Space h="xl" />
       <Stack gap="xl">
+        {kingdoms.length === 0 && <Text>No kingdoms found</Text>}
         {kingdoms.map((kingdom) => (
           <KingdomChecklistItem key={kingdom.id} kingdom={kingdom} />
         ))}
